bc8bc06ac86521c24dcbd9db3e969bb29a4ea629
Daniel Pekevski <dpekevski@gmail.com>
2022-01-24 23:22:37 +1100

    Update to tailwind v3

--- bitly-ball/tailwind.config.js
+++ bitly-ball/tailwind.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      'bitlyTitle': ['"Cookie"'],
+      bitlyTitle: ['"Cookie"'],
     },
     extend: {
       animation: {
-        bounce200: 'bounce 1s infinite 200ms',
-        bounce400: 'bounce 1s infinite 400ms',
-    },
+        bounce200: "bounce 1s infinite 200ms",
+        bounce400: "bounce 1s infinite 400ms",
+      },
     },
   },
   variants: {
     extend: {
-      opacity: ['disabled'],
-      animation: ['hover'],
+      opacity: ["disabled"],
+      animation: ["hover"],
     },
   },
   plugins: [],
-}
+};
