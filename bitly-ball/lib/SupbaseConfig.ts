3cd906b438ba1d992079d2b0e260a52da497c041
Daniel Pekevski <daniel.pekevski@stileeducation.com>
2022-04-14 17:20:44 +1000

    Fix authentication env variables

--- bitly-ball/lib/SupbaseConfig.ts
+++ bitly-ball/lib/SupbaseConfig.ts
@@ -1,12 +1,2 @@
-import { createClient } from "@supabase/supabase-js";
 import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs';
-
-const SUPABASE_URL: string = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const SUPABASE_KEY: string = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
-
-// export const supabase = createClient(
-//   SUPABASE_URL,
-//   SUPABASE_KEY
-// )
-
 export const supabase = supabaseClient;
